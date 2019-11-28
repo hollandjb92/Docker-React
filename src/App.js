37ffd594304d4621c8859ddfbb2549a82286ac9f
hollandjb <hollandjb@vcu.edu>
2019-11-27 19:06:23 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> HELLO WORLD</p>{" "}
+        <p> I WAS CHANGED ON THE FEATURE BRANCH </p>{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
